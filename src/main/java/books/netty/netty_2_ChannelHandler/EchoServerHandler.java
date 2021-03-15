8094757f32d5e2c85a44413da41725d0fb415256
chenyuqun <chenyuqun@playcrab.com>
2021-03-15 14:45:56 +0800

    【netty实战】:第2章 你的第一款netty应用程序

--- src/main/java/books/netty/netty_2_ChannelHandler/EchoServerHandler.java
+++ src/main/java/books/netty/netty_2_ChannelHandler/EchoServerHandler.java
@@ -1,8 +1,40 @@
 package books.netty.netty_2_ChannelHandler;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
 /**
  * @author chenyuqun
  * @date 2021/3/15 2:11 下午
  */
-public class EchoServerHandler {
+
+// 标记一个ChannelHandler可以被多个Channel安全地共享
+// 服务器是事件的接收方，所以要处理的是入站事件。
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received : " + in.toString(CharsetUtil.UTF_8));
+        // 将消息写给发送者，而不冲刷出站信息。
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        // 将未决消息冲刷到远程节点，并且关闭改channel.
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
